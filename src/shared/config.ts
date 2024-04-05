bb3e664d146f6e84e226bba28f57e1a4df77466f
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-04-06 02:14:58 +0530

    file name updates

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,10 +2,12 @@ interface Config {
   ADMIN_EMAIL: string;
   NEXTAUTH_SECRET: string;
   AUTH_EXPIRES: string;
+  REDIRECT_URL: string;
 }
 
 export const config: Config = {
   ADMIN_EMAIL: process.env.ADMIN_EMAIL ?? "",
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
   AUTH_EXPIRES: process.env.AUTH_EXPIRES ?? "7d",
+  REDIRECT_URL: process.env.REDIRECT_URL ?? "",
 };
