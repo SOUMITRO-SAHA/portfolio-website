7a7a3a8c171ae27f626f12ae71c6bdfcafdd26be
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-04-07 20:25:23 +0530

    User Page UI Complete

--- src/app/(protected)/admin/user/page.tsx
+++ src/app/(protected)/admin/user/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import AdminPageWrapper from "@/components/PageWrapper/AdminPageWrapper";
+import { AdminPageWrapper } from "@/components/PageWrapper";
 import { Button } from "@/components/ui/button";
 import {
   Form,
