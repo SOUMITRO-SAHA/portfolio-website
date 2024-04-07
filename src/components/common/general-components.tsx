0b4bd4ad82f1dd1354123e92c16a09c59c4e08cd
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-04-07 20:33:13 +0530

    UPDATE: Adding `Title`, `Divider`, `P`

--- src/components/common/general-components.tsx
+++ src/components/common/general-components.tsx
@@ -1,7 +1,15 @@
 import { cn } from "@/utils";
 
 export const Title = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className="text-lg font-semibold">{children}</div>;
+};
+
+export const Divider = () => {
+  return <div className="my-6" />;
+};
+
+export const Section = ({ children }: { children: React.ReactNode }) => {
+  return <section className="">{children}</section>;
 };
 
 export const H1 = ({
@@ -24,4 +32,8 @@ export const H2 = ({ children }: { children: React.ReactNode }) => {
 
 export const H3 = ({ children }: { children: React.ReactNode }) => {
   return <h3>{children}</h3>;
+};
+
+export const P = ({ children }: { children: React.ReactNode }) => {
+  return <p className="leading-relaxed text-zinc-300">{children}</p>;
 };
