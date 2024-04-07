4b69fe34caddf996887daff8da076febfac37395
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-04-07 20:40:33 +0530

    Implemented : Custom Date Picker

---
+++ src/components/common/date-picker/date-picker.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import * as React from "react";
+
+interface DatePickerProps {
+  date: Date | undefined;
+  setDate: (date: Date | undefined) => void;
+}
+
+export const DatePicker: React.FC<DatePickerProps> = (props) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          className={`w-[280px] justify-start text-left font-normal ${!props.date && "text-muted-foreground"}`}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {props.date ? format(props.date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        {/* Assuming Calendar component is imported */}
+        <Calendar
+          mode="single"
+          selected={props.date}
+          onSelect={props.setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
