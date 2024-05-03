858634c1873ba64edebfec77438f03d70772abb5
SOUMITRA SAHA <soumitrosahaofficial@gmail.com>
2024-05-03 22:44:20 +0530

    Active Tab bg-colour Implemented

--- src/components/sidebar/Menus.tsx
+++ src/components/sidebar/Menus.tsx
@@ -1,13 +1,17 @@
-import { cn } from "@/utils";
 import type { MenuItem } from "@/assets";
+import { cn } from "@/utils";
 import Link from "next/link";
-import React from "react";
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { type RootState } from "@/store/store";
+import { setActiveTab } from "@/store/slice/appSlice";
 
 interface MenuProps {
   id: number;
   link: string;
   icon: React.ReactNode;
   name: string;
+  active: boolean;
 }
 
 interface MenusProps {
@@ -15,16 +19,27 @@ interface MenusProps {
 }
 
 export const Menus: React.FC<MenusProps> = ({ menuItems }) => {
+  const { app } = useSelector((state: RootState) => state.app);
+  const dispatch = useDispatch();
+
+  // Function to handle tab click
+  const handleTabClick = (tabIndex: number) => {
+    dispatch(setActiveTab(tabIndex));
+  };
+
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-1">
       {menuItems.map((item) => (
-        <MenuItemComponent
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          link={item.link}
-          icon={item.icon}
-        />
+        <div onClick={() => handleTabClick(item.id)}>
+          <MenuItemComponent
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            link={item.link}
+            icon={item.icon}
+            active={app.activeTab === item.id}
+          />
+        </div>
       ))}
     </div>
   );
@@ -35,12 +50,15 @@ export const MenuItemComponent: React.FC<MenuProps> = ({
   link,
   icon,
   name,
+  active,
 }) => {
   return (
     <Link href={link} key={id}>
       <div
         className={cn(
-          "grid cursor-pointer grid-cols-12 items-center gap-2 p-1 px-3 text-sm leading-loose hover:bg-primary-foreground dark:hover:bg-slate-700",
+          "dark:highlight-white/20 dark:highlight-white/20 mx-1 grid h-12 w-full cursor-pointer grid-cols-12 items-center justify-center gap-2 rounded-lg bg-slate-900 p-2 px-3 text-sm font-semibold leading-loose text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2   focus:ring-offset-slate-50 dark:bg-transparent sm:w-auto",
+          active && "dark:bg-sky-500",
+          !active && "hover:bg-primary-foreground dark:hover:bg-sky-500/40",
         )}
       >
         {icon && <div className={cn(icon ? "col-span-2" : "")}>{icon}</div>}
