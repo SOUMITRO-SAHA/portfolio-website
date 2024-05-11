af3530e563d7879ea0f21b075115cacc1165b08d
SOUMITRA SAHA <soumitrosahaofficial@gmail.com>
2024-05-11 17:21:52 +0530

    Config File Updated

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,6 +3,7 @@ interface Config {
   NEXTAUTH_SECRET: string;
   AUTH_EXPIRES: string;
   REDIRECT_URL: string;
+  NEXT_PUBLIC_HASHNODE_ENDPOINT: string;
 }
 
 export const config: Config = {
@@ -10,4 +11,6 @@ export const config: Config = {
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
   AUTH_EXPIRES: process.env.AUTH_EXPIRES ?? "7d",
   REDIRECT_URL: process.env.REDIRECT_URL ?? "",
+  NEXT_PUBLIC_HASHNODE_ENDPOINT:
+    process.env.NEXT_PUBLIC_HASHNODE_ENDPOINT ?? "",
 };
